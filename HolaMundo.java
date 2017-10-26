adff06ab3370904bb720648fc1d2cafa5045e7a3
manolo molino <programacionDAMVC@gmail.com>
2017-10-26 14:15:09 +0200

    modificado:    HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -15,26 +15,6 @@ public class HelloWorld //clase HolaMundo
     System.out.println("hola Mfdfdfundo dfdfdf"); //mostra en consola
     System.out.printf("%s%n" , "Hello Wodfdfdfrld"); //mostra en consola  
       
-      try
-      {
-          //FileInputStream to read the file
-          fis = new FileInputStream(file);
-
-          /*Passed the FileInputStream to BufferedInputStream
-           *For Fast read using the buffer array.*/
-          bis = new BufferedInputStream(fis);
-
-          /*available() method of BufferedInputStream
-           * returns 0 when there are no more bytes
-           * present in the file to be read*/
-          while( bis.available() > 0 ){             	
-              System.out.print((char)bis.read());
-          }
-
-       }catch(FileNotFoundException fnfe)
-        {
-            System.out.println("The specified file not found" + fnfe);
-        }
         catch(IOException ioe)
         {
             System.out.println("I/O Exception: " + ioe); 
