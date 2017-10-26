d0dbc909b402202292dae3dcdfe0a4a016cb1ee5
manolo molino <programacionDAMVC@gmail.com>
2017-10-26 13:40:52 +0200

    second commit 	nuevo archivo: .gitignore 	nuevo archivo: HolaMundo.java 	modificado:    README.md

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+//Programa HolaMundo.java
+/** Programa típico de
+    hola mundo, para comprobar 
+    el funcionamiento del compilador
+    y la JVM
+    @author Manuel Molino
+    @version 1.0
+*/
+
+
+public class HolaMundo //clase HolaMundo
+{
+  public static void main(String[] args) //metodo principal
+  {
+    System.out.println("hola Mundo"); //mostra en consola
+  }  
+} 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# prueba2
+# repositorio prueba2
+
+## Objetivos
+
+- Crear un repositorio en gitHub
+- Sincronizarlo con un repositorio local
+- Subir contenido a gitHub
+- Bajar cambios de gitHub
