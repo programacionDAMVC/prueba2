c028542b4d8cc1e763cdfa5b5dcb50c2336abc02
programacionDAMVC <32073654+programacionDAMVC@users.noreply.github.com>
2017-10-26 14:13:59 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,16 +3,54 @@
     hola mundo, para comprobar 
     el funcionamiento del compilador
     y la JVM
-    @author Manuel Molino
-    @version 1.0
+    @author Manolo Molino
+    @version 1.0.1
 */
 
 
-public class HolaMundo //clase HolaMundo
+public class HelloWorld //clase HolaMundo
 {
   public static void main(String[] args) //metodo principal
   {
-    System.out.println("hola Mundo"); //mostra en consola
-    System.out.println("Hello World"); //mostra en consola  
+    System.out.println("hola Mfdfdfundo dfdfdf"); //mostra en consola
+    System.out.printf("%s%n" , "Hello Wodfdfdfrld"); //mostra en consola  
+      
+      try
+      {
+          //FileInputStream to read the file
+          fis = new FileInputStream(file);
+
+          /*Passed the FileInputStream to BufferedInputStream
+           *For Fast read using the buffer array.*/
+          bis = new BufferedInputStream(fis);
+
+          /*available() method of BufferedInputStream
+           * returns 0 when there are no more bytes
+           * present in the file to be read*/
+          while( bis.available() > 0 ){             	
+              System.out.print((char)bis.read());
+          }
+
+       }catch(FileNotFoundException fnfe)
+        {
+            System.out.println("The specified file not found" + fnfe);
+        }
+        catch(IOException ioe)
+        {
+            System.out.println("I/O Exception: " + ioe); 
+        }
+        finally
+        {
+            try{
+               if(bis != null && fis!=null)
+               {
+       	          fis.close();
+                  bis.close();
+               }      
+             }catch(IOException ioe)
+              {
+                  System.out.println("Error in InputStream close(): " + ioe);
+              }         
+        }
   }  
 } 
