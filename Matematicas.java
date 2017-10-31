76a37dd4aceac1149f822f0f8690d0457b312d9c
manolo molino <programacionDAMVC@gmail.com>
2017-10-31 10:42:21 +0100

    nuevo archivo: Matematicas.java 	nuevo archivo: TestMatematicas.java

---
+++ Matematicas.java
@@ -0,0 +1,8 @@
+public class Matematicas {
+
+    public static boolean esPar (int numero) {
+        return numero % 2 == 0;
+    }
+
+    //nuevos metodos
+}
---
+++ TestMatematicas.java
@@ -0,0 +1,9 @@
+public class TestMatematicas {
+    public static void main(String args[]){
+
+       int numeroLeido = 170;
+        boolean resultado = Matematicas.esPar(numeroLeido);
+        System.out.println(numeroLeido + " ¿es par? " + resultado);
+
+    }
+}
